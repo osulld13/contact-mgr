3133b82180f900a4618c617c2e90adef88cd8fb2
osulld13 <osulld13@tcd.ie>
2020-02-13 08:11:55 +0000

    Added save method. Updated the buil'dgradle

--- src/main/java/is/osullivan/contactmgr/Application.java
+++ src/main/java/is/osullivan/contactmgr/Application.java
@@ -1,5 +1,7 @@
 package is.osullivan.contactmgr;
 
+import is.osullivan.contactmgr.model.Contact;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -8,7 +10,7 @@ import org.hibernate.service.ServiceRegistry;
 
 public class Application {
     // Hold a reusable reference to a session factory
-    private static final SessionFactory sessioFactory = buildSessionFactory();
+    private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
         // Create a StandardServiceRegistry
@@ -17,6 +19,19 @@ public class Application {
     }
 
     public static void main(String[] args) {
-
+        Contact contact = new Contact.ContactBuilder("Donal", "O'Sullivan")
+                .withPhone(12341234L)
+                .withEmail("asdfasfd")
+                .build();
+        // Open a session
+        Session session = sessionFactory.openSession();
+        // Begin a transaction
+        session.beginTransaction();
+        // Use the session to save the contact
+        session.save(contact);
+        // Commit the transaction
+        session.getTransaction().commit();
+        // Close the session
+        session.close();
     }
 }
