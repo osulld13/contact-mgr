325a2f9939ef6aa81933c51e4ec40e3a798ad208
osulld13 <osulld13@tcd.ie>
2020-02-16 15:51:45 +0000

    Extended application with new methods for contacts

--- src/main/java/is/osullivan/contactmgr/Application.java
+++ src/main/java/is/osullivan/contactmgr/Application.java
@@ -1,12 +1,15 @@
 package is.osullivan.contactmgr;
 
 import is.osullivan.contactmgr.model.Contact;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 import org.hibernate.service.ServiceRegistry;
+
+import java.util.List;
 
 public class Application {
     // Hold a reusable reference to a session factory
@@ -23,15 +26,70 @@ public class Application {
                 .withPhone(12341234L)
                 .withEmail("asdfasfd")
                 .build();
+        int id = save(contact);
+        // Display a list of contacts before update
+        System.out.printf("%n%nBefore update%n%n");
+        fetchAllContacts().forEach(System.out::println);
+        // Get the persisted contact
+        Contact contactToBeUpdated = findContactById(id);
+        // Update the contact
+        contactToBeUpdated.setFirstname("NewName");
+        // Persist the changes
+        System.out.printf("%n%nUpdating...%n%n");
+        update(contactToBeUpdated);
+        System.out.printf("%n%nUpdate Complete!%n%n");
+        // Display a list of contacts after update
+        fetchAllContacts().forEach(System.out::println);
+    }
+
+    private static Contact findContactById(int id) {
+        // Open a session
+        Session session = sessionFactory.openSession();
+        // Retrieve the contact
+        Contact contact = session.get(Contact.class, id);
+        // Close the session
+        session.close();
+        // Return the object
+        return contact;
+    }
+
+    private static void update (Contact contact) {
+        // Open a session
+        Session session = sessionFactory.openSession();
+        // Create transaction
+        session.beginTransaction();
+        // Update contact
+        session.update(contact);
+        // Commit the transaction
+        session.getTransaction().commit();
+        // Close the session
+        session.close();
+    }
+
+    @SuppressWarnings("unchecked assignment")
+    private static List<Contact> fetchAllContacts() {
+        // Open a session
+        Session session = sessionFactory.openSession();
+        // Create criteria
+        Criteria criteria = session.createCriteria(Contact.class);
+        // Get list of objects using the criteria
+        List<Contact> contacts = criteria.list();
+        // CLose a session
+        session.close();
+        return contacts;
+    }
+
+    private static int save(Contact contact) {
         // Open a session
         Session session = sessionFactory.openSession();
         // Begin a transaction
         session.beginTransaction();
         // Use the session to save the contact
-        session.save(contact);
+        int id = (int) session.save(contact);
         // Commit the transaction
         session.getTransaction().commit();
         // Close the session
         session.close();
+        return id;
     }
 }
